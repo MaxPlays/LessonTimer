2921d24128bca14d9008441fd64cfc550e37b945
MaxPlays <maxplays88@gmail.com>
2018-05-18 14:42:52 +0200

    Added license texts

--- master.js
+++ master.js
@@ -1,3 +1,21 @@
+/*
+
+Copyright 2018 Maximilian Negedly
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
 //var a = prompt("Enter end time").trim().split("-");
 var before = null;
 var to = null;
@@ -44,6 +62,6 @@ function getInfo(){
   }else if((h == 8 && m <= 59) | (h == 9 && m < 45)){
 
   }else if(h == 9 && m <= 59){
-    
+
   }
 }
